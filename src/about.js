fc36ddd228db432a0a77c37baf10b595380fc0a5
antoniGIT2403 <antonimalki1990@gmail.com>
2021-03-20 10:58:37 +0100

    modif final8

--- src/about.js
+++ src/about.js
@@ -14,14 +14,7 @@ function About(props) {
     </div>
     <div className="container">
       <p className="about-maintext">
-      lNam aut opta doluptur? Ure nest audistiur, estiure dolore
-cum esti quis pa ipsam, cus cusdanis minis suntiae nonsequos
-et, quosti omnis sit, ute simperi oreperatem eum volorrum
-ati ut faccullupti comnihil imporpostio comnis aturiasita
-as dolores evenitasint volorum as et quia qui odis es sitaqui
-atet volenderit, sitatio tem accus ea velia sum, vel idellab
-orporem quuntemoles as dus eosae dis il es mos nemque il ma
-doluptate necatur arisquas etur rat et eliquidi dita exer
+     Leni « LKIM» Malki est un artiste, designer et rappeur originaire d’île de France. Jeune auteur, diplômé mention TB de l’ESAG Penninghen, il signe avec « Bledard sur Seine » son premier album de bande dessinée aux editions Michel Lafon. En 2018, il occupait le poste de directeur artistique junior chez Paco Rabanne et le groupe Puig ( Jean Paul Gaultier, Nina Ricci, ). Il illustre également la serie de Bd « Comme on Peut » avec Jhon Rachid « Comme on peut », dont le 3 eme volet confirme son talent de narrateur. Egalement, rappeur et compositeur du groupe Marabou avec qui il a sorti 2 albums « Les Chroniques de Maraboutown, et Maraboutown ».Il gère la production des clips et la direction artistique du groupe.
       </p>
       <p className="about-secondtext">
       lNam aut opta doluptur? Ure nest audistiur, estiure dolore
